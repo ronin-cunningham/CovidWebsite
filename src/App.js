aa07e29fe1e0fcef143477290fd7631bb42fc0e2
Ronin Cunningham <ronincunningham1@gmail.com>
2020-03-21 16:43:54 -0700

    Made more progress
    *organized folder structure
    
    * did a bunch of stuff ~~ added react-router-dom, able to navigate webapp now. Added Navbar, etc.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
 import "./App.css";
+import Container from "react-bootstrap/Container";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Home } from "./home/Home";
+import { ResearchReHashed } from "./research-re-hashed/ResearchReHashed";
+import { Faq } from "./faq/Faq";
+import { Contact } from "./contact/Contact";
+import { NoMatch } from "./components/NoMatch";
+import { NavigationBar } from "./components/NavigationBar";
 
 function App() {
   return (
-    <div className="App">
-      <Button>hi</Button>
-    </div>
+    <React.Fragment>
+      <NavigationBar />
+      <Container>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/researchrehashed" component={ResearchReHashed} />
+            <Route path="/faq" component={Faq} />
+            <Route path="/contact" component={Contact} />
+            <Route component={NoMatch} />
+          </Switch>
+        </Router>
+      </Container>
+    </React.Fragment>
   );
 }
 
---
+++ src/research-re-hashed/ResearchReHashed.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const ResearchReHashed = () => {
+  return <div>rrh</div>;
+};
